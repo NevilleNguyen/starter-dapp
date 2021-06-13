a2a29cf20aa0c0d4448f7ccfbde349a00f50c4b1
NevilleNguyen <quangnguyen.dfse@gmail.com>
2021-06-13 17:45:23 +0700

    change back to origin

--- react-delegationdashboard/src/config.mainnet.ts
+++ react-delegationdashboard/src/config.mainnet.ts
@@ -37,7 +37,7 @@ export const network: NetworkType = {
   apiAddress: 'https://api.elrond.com',
   gatewayAddress: 'https://gateway.elrond.com',
   explorerAddress: 'https://explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqylllslmq6y6',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhllllsajxzat',
 };
 
 const networkSchema = object({
